9eac08272b891a970523e2caa352ddb0805769c4
180579045 <msn103125@126.com>
2017-02-22 10:14:27 +0800

    add class prj

--- Class_Learn1/Sales_data.cpp
+++ Class_Learn1/Sales_data.cpp
@@ -1,2 +1,8 @@
 #include "stdafx.h"
 #include "Sales_data.h"
+
+double Str_Sales_data::Average_Price()
+{
+	double ret = 0;
+	return ret;
+}
--- Class_Learn1/Sales_data.h
+++ Class_Learn1/Sales_data.h
@@ -2,11 +2,33 @@
 #ifndef SALES_DATA_H
 #define  SALES_DATA_H
 
+#include <string>
+#include "stdafx.h"
+
+using namespace std;
+using std::string;
+
 class Sales_data
 {
 public:
 protected:
 private:
+};
+
+struct Str_Sales_data
+{
+	// 方法成员;
+	string GetIsbn()         // 获取Isbn;
+	{
+		return Book_Isbn;
+	}
+	double Average_Price();  // 获取平均价格;
+	
+	// 数据成员;
+	string Book_Isbn;        // 图书编号;
+	unsigned int NumOfSale;  // 销售总量;
+	double TotalIncome;      // 销售总额;
+
 };
 
 #endif
