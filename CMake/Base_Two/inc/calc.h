d07c5314cf1b9bb04fd17c83e08affd860d6d969
visiontrail <msn103125@126.com>
2019-02-14 15:32:57 +0800

    update cmake add library file

---
+++ CMake/Base_Two/inc/calc.h
@@ -0,0 +1,15 @@
+#ifndef _CALC_
+#define _CALC_
+
+#ifdef __cplusplus
+
+extern "C"
+{
+#endif
+    int Get_Max(int a, int b);
+#ifdef __cplusplus
+};
+
+#endif
+
+#endif
--- CMake/Base_Two/src/CmakeTry_1.cpp
+++ CMake/Base_Two/src/CmakeTry_1.cpp
@@ -4,6 +4,7 @@
 #include "cmake_try.h"
 #include <iostream>
 #include "hello.h"
+#include "calc.h"
 using namespace std;
 
 int main()
@@ -11,12 +12,12 @@ int main()
 	int temp;
 	func(&temp);
 	cout << temp << endl;
+	cout << "The Max Number is" << Get_Max(10, 20) << endl;
 	printhello();
-    return 0;
+	return 0;
 }
 
 void func(int *a)
 {
 	*a = PRINTDEFINE;
 }
-
