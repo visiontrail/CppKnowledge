4ec187ba74dd4217b07e00cbba551b67fff3b155
user.name <msn103125@126.com>
2019-03-21 16:25:53 +0800

    update makefile

--- Make/Makefile/Makefile_Use_So/inc/calc2.h
+++ Make/Makefile/Makefile_Use_So/inc/calc2.h
@@ -2,14 +2,17 @@
 #define _CALC2_
 
 #ifdef __cplusplus
-
 extern "C"
 {
 #endif
+
+
     int Get_Min(int a, int b);
+    int Get_SomeUndef();
+
+
 #ifdef __cplusplus
 };
-
 #endif
 
 #endif
--- Make/Makefile/Makefile_Use_So/main.cpp
+++ Make/Makefile/Makefile_Use_So/main.cpp
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <iostream>
-#include "cmake_try.h"
 #include "calc2.h"
 
 using namespace std;
@@ -11,6 +10,7 @@ int main(int argc, char **argv)
     int ret = 0;
     // 这里直接调用了一个不带后缀名的shared library文件，但是没法用……
     //func2(&ret);
-    cout << "ret is " << ret << endl;
-    cout << "GetMin: " << Get_Min(10,20) << endl;
+    Get_Min(10,20);
+    std::cout << "ret is " << ret << std::endl;
+    std::cout << "GetMin: " << Get_Min(10,20) << std::endl;
 }
