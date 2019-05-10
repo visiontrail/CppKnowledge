63c8070c5f65225dc834ed0c6d95b23b58d2a1f9
user.name <msn103125@126.com>
2019-05-10 18:36:50 +0800

    update 19510

---
+++ CppBasic/Class_BaseKnowledge/src/Macro_definition.cpp
@@ -0,0 +1,14 @@
+/*
+ * Windows平台下的默认宏定义
+*/
+#if defined( _MSC_VER )
+	#if !defined( _CRT_SECURE_NO_WARNINGS )
+		#define _CRT_SECURE_NO_WARNINGS		// This test file is not intended to be secure.
+	#endif
+#endif
+
+
+int main( int argc, const char ** argv )
+{
+
+}
---
+++ Usage/Timer/PreTest/use_alarm.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+/*
+ * 使用alarm函数，对系统信号SIGALRM进行设置：
+ * 函数原型：unsigned int alarm（unsigned int seconds）
+ * 特性：alarm函数后一次设定将取消前一次的设定，如果为0的话，系统则不再发送SIGALARM信号
+*/
+
+int count;
+void sigalrm_func(int sig)
+{
+    count++;
+    printf("alarm![%d]\n", count);
+
+    alarm(10);
+    printf("over\n");
+    return;
+}
+
+int main(void)
+{
+    signal(SIGALRM, sigalrm_func); // step1:注册一个信号量
+    alarm(10);                     // step2:启动这个信号量，时间为10秒
+    printf("start frist sigalarm\n");
+
+    while (1)
+    {
+        sleep(1);
+        printf(".");
+        fflush(stdout);
+    }
+}
